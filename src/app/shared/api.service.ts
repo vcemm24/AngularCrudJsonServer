8ef9321924804ab009f5fe398a711e963cdcbe03
vcemm24 <vcemm24@gmail.com>
2022-09-30 22:19:44 +0100

    end of form and crud op

---
+++ src/app/shared/api.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  poststudent(data:any){
+    return this.http.post<any>("http://localhost:3000/posts",data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  getstudent(){
+    return this.http.get<any>("http://localhost:3000/posts")
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  updatestudent(data:any,id:number){
+    return this.http.put("http://localhost:3000/posts/"+id,data)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+  deletestudent(id:number){
+    return this.http.delete<any>("http://localhost:3000/posts/"+id)
+    .pipe(map((res:any)=>{
+      return res;
+    }))
+  }
+
+
+
+}
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { ApiService } from '../shared/api.service';
+import { studentdata } from './student.model';
 
 @Component({
   selector: 'app-student',
@@ -8,10 +10,13 @@ import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 })
 export class StudentComponent implements OnInit {
 
+  studentmodel:studentdata=new studentdata
+
   showadd!:boolean;
   showupdate!:boolean;
   formValue!:FormGroup;
-  constructor(private formBuilder:FormBuilder) { }
+  allstudentdata:any;
+  constructor(private formBuilder:FormBuilder,private api:ApiService) { }
 
   ngOnInit(): void {
     this.formValue = this.formBuilder.group({
@@ -20,15 +25,74 @@ export class StudentComponent implements OnInit {
       mobile:['',Validators.required],
       city:['',Validators.required]
     })
+    this.getdata();
   }
 
   add(){
     this.showadd = true;
     this.showupdate = false;
   }
-  update(){
+  edit(data:any){
     this.showadd = false;
     this.showupdate = true;
+    this.studentmodel.id = data.id;
+
+    this.formValue.controls['name'].setValue(data.name);
+    this.formValue.controls['email'].setValue(data.email);
+    this.formValue.controls['mobile'].setValue(data.mobile);
+    this.formValue.controls['city'].setValue(data.city);
+  }
+
+  update(){
+    this.studentmodel.name = this.formValue.value.name;
+    this.studentmodel.email = this.formValue.value.email;
+    this.studentmodel.mobile = this.formValue.value.mobile;
+    this.studentmodel.city = this.formValue.value.city;
+
+    this.api.updatestudent(this.studentmodel,this.studentmodel.id).subscribe(
+      res=>{
+        this.formValue.reset();
+        this.getdata();
+        alert("Record updated successfully!");
+      },
+      err=>{
+        alert("something went wrong");
+      }
+    )
+  }
+
+  addstudent(){
+    this.studentmodel.name = this.formValue.value.name;
+    this.studentmodel.email = this.formValue.value.email;
+    this.studentmodel.mobile = this.formValue.value.mobile;
+    this.studentmodel.city = this.formValue.value.city;
+
+    this.api.poststudent(this.studentmodel).subscribe(res=>{
+      console.log(res)
+      this.formValue.reset()
+      this.getdata();
+      alert("record add success");
+    },
+    err=>{
+      alert("something went wrong!!!");
+    }
+    )
+  }
+
+  getdata(){
+    this.api.getstudent()
+    .subscribe(res=>{
+      this.allstudentdata = res;
+    })
+  }
+
+  deletestud(data:any){
+    if(confirm('Are you sure to delete?'))
+    this.api.deletestudent(data.id)
+    .subscribe(res=>{
+      alert("Record deleted successfully");
+      this.getdata();
+    })
   }
 
 }
