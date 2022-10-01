5f29befacb4ac7c679a4d696b1c8484e75527d6d
vcemm24 <vcemm24@gmail.com>
2022-10-01 12:32:32 +0100

    end of sign config

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+loginform!:FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder,private router:Router,private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.loginform = this.formBuilder.group({
+      email:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
+
+  login(){
+    this.http.get<any>("http://localhost:3000/signup")
+    .subscribe(
+      res=>{
+        const user = res.find((a:any)=>{
+          return a.email === this.loginform.value.email && a.password === this.loginform.value.password
+        })
+        if(user){
+          alert("login success");
+          this.loginform.reset();
+          this.router.navigate(['student']);
+        }
+        else{
+          alert("user not found!")
+        }
+      },
+      err=>{
+        alert("something went wrong");
+      }
+
+    )
   }
 
 }
